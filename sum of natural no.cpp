e7ddf601e002bb2c3dd3bc81ad2b0307704dd68b
Suhas-108123128 <143410961+Suhas-108123128@users.noreply.github.com>
2023-10-03 14:42:07 +0530

    Add files via upload

---
+++ sum of natural no.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int i,n,s=0;
+	printf("Enter the value of n:");
+	scanf("%d",&n);
+	for(i=0;i<=n;i++)
+{
+	s=s+i;
+}
+printf("he sum of natural no is %d",s);
+return 0;
+	
+}
