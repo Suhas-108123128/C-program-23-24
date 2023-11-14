d54c51c5dcb9be611b247ef76093262a961d293f
Suhas-108123128 <143410961+Suhas-108123128@users.noreply.github.com>
2023-11-14 16:46:43 +0530

    Update freq str.cpp

--- freq str.cpp
+++ freq str.cpp
@@ -5,12 +5,14 @@ int main()
 	c=1;
 	max=0;
 	min=100;
+	char max1;
+	char min1;
 	char str1[100];
 	printf("enter the string:");
 	scanf("%[^\n]s",str1);
 	for(i=0;str1[i]!='\0';i++)
 	{
-		for(j=0;str1[i]!='\0';j++)
+		for(j=0;str1[j]!='\0';j++)
 		{
 		
 		if(str1[i]==str1[j] && i!=j);
@@ -20,16 +22,18 @@ int main()
 	{
 	
 	max=c;
+	max1=str1[i];
 	c=0;
 	}
 	else if(min>c)
 {
 
 	min=c;
+	min1=str1[i];
 	c=0;
 }
 	
 	
 	}
-	printf("most and least repeated characters are %d %d",max,min);
+	printf("most and least repeated characters are %c %c",max1,min1);
 }
